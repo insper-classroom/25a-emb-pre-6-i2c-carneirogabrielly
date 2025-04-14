6933061b683992472565e29d76af765cc907bcec
Gabrielly Carneiro <130596331+carneirogabrielly@users.noreply.github.com>
2025-04-14 09:59:20 -0300

    exercicio3

--- exe3/main.c
+++ exe3/main.c
@@ -13,6 +13,7 @@
 const int I2C_CHIP_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 20;
 const int I2C_SCL_GPIO = 21;
+const int BMP280_CHIP = 0x76;
 
 void i2c_task(void *p) {
     i2c_init(i2c_default, 400 * 1000);
@@ -23,6 +24,10 @@ void i2c_task(void *p) {
 
     // TODO
     // read id chip BMP280
+    uint8_t buffer[1];
+    uint8_t reg_address = 0xD0;
+    i2c_write_blocking(i2c_default, BMP280_CHIP, &reg_address, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, BMP280_CHIP, buffer, 1, false);
     printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
